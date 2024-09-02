cc15a697a5ceca53b832e85993c8e244aa4a27c4
Md. Pieash Ahmed <pieash9@gmail.com>
2024-09-02 17:07:33 +0600

    added employee controller and dependencies

--- src/main/java/com/pieash9/starter/controllers/EmployeeController.java
+++ src/main/java/com/pieash9/starter/controllers/EmployeeController.java
@@ -1,15 +1,22 @@
 package com.pieash9.starter.controllers;
 
 import com.pieash9.starter.dto.EmployeeDTO;
+import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 
 @RestController
 public class EmployeeController {
+    @GetMapping(path = "/employees/{id}")
+    public EmployeeDTO getEmployees(@PathVariable("id") Long employeeId) {
+        return new EmployeeDTO(employeeId, "John", LocalDate.of(2020, 1, 1), true);
+    }
+
     @GetMapping(path = "/employees")
-    public EmployeeDTO getEmployees() {
-        return new EmployeeDTO(1L, "John", LocalDate.of(2020, 1, 1), true);
+    public  String getData(@PathParam("sortBy") String sortBy, @PathParam("limit") Integer limit) {
+        return "Hello " + sortBy + " " + limit;
     }
 }
---
+++ src/main/java/com/pieash9/starter/entities/EmployeeEntity.java
@@ -0,0 +1,22 @@
+package com.pieash9.starter.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employees")
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Long id;
+    private  String name;
+    private LocalDate dateOfJoining;
+    private  boolean isActive;
+}
